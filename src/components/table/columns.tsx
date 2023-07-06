e90f461db70bd0a6f389512e1ec56313dd5c3631
Saif045 <saif.x45@gmail.com>
2023-07-06 03:00:10 +0300

    small bug fix

--- src/components/table/columns.tsx
+++ src/components/table/columns.tsx
@@ -6,7 +6,7 @@ import { Badge } from "@/components/ui/badge";
 import { Checkbox } from "@/components/ui/checkbox";
 
 import { priorities, statuses } from "./data";
-import { stock } from "@/app/page";
+import { stock } from "@/actions/getStocks";
 import { DataTableColumnHeader } from "./data-table-column-header";
 import { DataTableRowActions } from "./data-table-row-actions";
 import Image from "next/image";
